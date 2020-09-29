92db22bbbf400d36fe5f4ab91c4322163165a6c3
Molly <mziegenfelder@dtcc.com>
2020-09-29 17:58:50 -0400

    Add edit and delete functionality to list

--- src/app/insert-list/insert-list.component.ts
+++ src/app/insert-list/insert-list.component.ts
@@ -9,15 +9,37 @@ import { GroceryItem } from '../groceryList'
 export class InsertListComponent implements OnInit {
 groceryList: GroceryItem[] = [];
 item; amount; unit;
+editItem; editAmount; editUnit;
+selectedItem: GroceryItem;
 
-addItem(form){
+addItem(form): void{
     console.log('add item');
-    console.log(this.item);
     this.groceryList.push({item : this.item, amount : this.amount, unit : this.unit});
-    for(let i = 0; i < this.groceryList.length; i++){
-      console.log(this.groceryList[i].item);
-    }
     form.reset();
+  }
+
+  onSelect(i: GroceryItem): void{//allow to edit
+    this.selectedItem = i;
+    this.editItem = this.selectedItem.item;
+    this.editAmount = this.selectedItem.amount;
+    this.editUnit = this.selectedItem.unit;
+  }
+
+  deleteItem(): void{
+    const index = this.groceryList.indexOf(this.selectedItem);
+    if(index > -1){//validate item has an index in the array
+      this.groceryList.splice(index, 1);//remove from array
+    }
+  }
+
+  saveItem(): void{
+    const index = this.groceryList.indexOf(this.selectedItem);
+    if(index > -1){//validate item has an index in the array
+      this.groceryList[index].item=this.editItem;
+      this.groceryList[index].amount=this.editAmount;
+      this.groceryList[index].unit=this.editUnit;
+    }
+    this.selectedItem = null;//remove editing
   }
 
 constructor() { }
